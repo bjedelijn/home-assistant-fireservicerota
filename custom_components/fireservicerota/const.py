f6609ba1f8d23ebb423be25a0c9bef1cbc96421b
bjedelijn <119961398+bjedelijn@users.noreply.github.com>
2026-09-09 13:16:22 +0200

    extended: add pager service constants

--- custom_components/fireservicerota/const.py
+++ custom_components/fireservicerota/const.py
@@ -10,3 +10,13 @@ WSS_BWRURL = "wss://{0}/cable?access_token={1}"
 
 DATA_CLIENT = "client"
 DATA_COORDINATOR = "coordinator"
+
+SERVICE_SEND_PAGER_MESSAGE = "send_pager_message"
+
+ATTR_ENTRY_ID = "entry_id"
+ATTR_PAGER_ID = "pager_id"
+ATTR_MESSAGE = "message"
+ATTR_ADDRESS = "address"
+ATTR_ADDRESSES = "addresses"
+ATTR_CONFIRMATION = "confirmation"
+ATTR_WEBHOOK_URL = "webhook_url"
